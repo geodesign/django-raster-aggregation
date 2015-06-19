a89a76da0d43c8a821ba62a928bb2e6e64006a6e
Daniel Wiesmann <daniel.wiesmann@gmail.com>
2015-06-19 17:10:02 +0100

    More detailed testing

--- tests/test_aggregation.py
+++ tests/test_aggregation.py
@@ -4,6 +4,8 @@ import os
 import shutil
 import tempfile
 
+import numpy
+
 from django.core.files import File
 from django.test import TestCase
 from raster.models import RasterLayer
@@ -52,13 +54,35 @@ class RasterAggregationTests(TestCase):
     def test_count_value_count_results(self):
         self.assertEqual(ValueCountResult.objects.all().count(), 2)
 
-    def test_count_values(self):
-        results = ValueCountResult.objects.all()
+    def test_count_values_for_st_petersburg(self):
+        result = ValueCountResult.objects.get(aggregationarea__name='St Petersburg')
         self.assertEqual(
-            json.loads(results[0].value),
+            json.loads(result.value),
             {"0": 50036, "1": 536, "2": 53, "3": 4044, "4": 31134, "8": 1214, "9": 2674}
         )
+
+    def test_count_values_for_coverall(self):
+        # Get result
+        result = ValueCountResult.objects.get(aggregationarea__name='Coverall')
+        result = json.loads(result.value)
+        # Compute expected totals from numpy value count
+        expected = {}
+        for tile in self.rasterlayer.rastertile_set.filter(tilez=11):
+            val, counts = numpy.unique(tile.rast.bands[0].data(), return_counts=True)
+            for pair in zip(val, counts):
+                if str(pair[0]) in expected:
+                    expected[str(pair[0])] += pair[1]
+                else:
+                    expected[str(pair[0])] = pair[1]
+        # Assert totals are correct
         self.assertEqual(
-            json.loads(results[1].value),
-            {"0": 111138, "1": 700, "2": 53, "3": 4080, "4": 31632, "8": 1300, "9": 2845}
+            sum(result.values()),
+            sum(expected.values()) + (111138 - expected["0"])
         )
+        # Remove nodata value - the tiles are not entirely
+        # covered by the coverall geom.
+        expected.pop("0")
+        # Assert the nodata value is as expected
+        self.assertEqual(result.pop("0"), 111138)
+        # Assert value counts are correct
+        self.assertEqual(result, expected)
---
+++ tests/test_parser.py
@@ -0,0 +1,52 @@
+import inspect
+import os
+import shutil
+import tempfile
+
+from django.core.files import File
+from django.test import TestCase
+from raster_aggregation.models import AggregationLayer
+
+
+class RasterAggregationTests(TestCase):
+
+    def setUp(self):
+        # Instantiate Django file instances with nodes and links
+        self.pwd = os.path.dirname(
+            os.path.abspath(
+                inspect.getfile(inspect.currentframe())
+            )
+        )
+
+        shapefile = File(open(os.path.join(self.pwd, 'data/shapefile.zip')))
+
+        self.media_root = tempfile.mkdtemp()
+
+        with self.settings(MEDIA_ROOT=self.media_root):
+
+            # Create aggregation layer
+            self.agglayer = AggregationLayer.objects.create(
+                name='abc',
+                name_column='name',
+                shapefile=shapefile
+            )
+            # Parse aggregation layer
+            self.agglayer.parse()
+
+    def tearDown(self):
+        shutil.rmtree(self.media_root)
+
+    def test_nr_of_aggregation_areas_created(self):
+        self.assertEqual(self.agglayer.aggregationarea_set.count(), 2)
+
+    def test_aggregation_area_properties(self):
+        self.assertTrue(self.agglayer.aggregationarea_set.get(name='St Petersburg'))
+        self.assertTrue(self.agglayer.aggregationarea_set.get(name='Coverall'))
+
+    def test_parse_log_was_written(self):
+        self.assertTrue(
+            'Started parsing Aggregation Layer' in self.agglayer.parse_log
+        )
+        self.assertTrue(
+            'Finished parsing shapefile' in self.agglayer.parse_log
+        )
