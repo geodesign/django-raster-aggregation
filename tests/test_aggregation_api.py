65652fffafcb1f5bd978ae8931305d1fcaffc179
Daniel Wiesmann <daniel.wiesmann@gmail.com>
2016-01-12 10:54:58 +0000

    Added test for aggregation based on json input.

--- tests/test_aggregation_api.py
+++ tests/test_aggregation_api.py
@@ -2,6 +2,7 @@ import json
 
 from django.core.urlresolvers import reverse
 from django.test import Client
+from django.utils.http import urlquote
 from raster_aggregation.models import AggregationArea, ValueCountResult
 
 from .aggregation_testcase import RasterAggregationTestCase
@@ -109,6 +110,25 @@ class RasterAggregationApiTests(RasterAggregationTestCase):
 
         # Assert value count result was created
         self.assertTrue(ValueCountResult.objects.filter(aggregationarea=self.area).exists())
+
+    def test_aggregation_api_legend_json_grouping(self):
+        # Request result
+        response = self.client.get(
+            self.url + '?layers=a={0}&formula=a&grouping={1}'
+            .format(self.rasterlayer.id, urlquote(self.legend_exp.json))
+        )
+        self.assertEqual(response.status_code, 200)
+
+        # Assert result has the right aggregationarea id
+        self.assertEqual(json.loads(response.content.strip().decode())['id'], self.area.id)
+
+        # Parse result
+        result = json.loads(response.content.strip().decode())['value']
+
+        expected = {'(x >= 2) & (x < 5)': self.expected['2'] + self.expected['3'] + self.expected['4']}
+
+        # Assert all data values are according to the formula
+        self.assertDictEqual(result, expected)
 
     def test_value_count_for_raster_with_missing_tile(self):
         url = reverse('aggregationareavalue-list')
