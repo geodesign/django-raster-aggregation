bf628a7ee4958fc502e3376b3007ae1042392e71
Daniel Wiesmann <daniel.wiesmann@gmail.com>
2017-05-28 11:29:46 +0100

    Changed error code for missing query parameter exception to 400.

--- raster_aggregation/exceptions.py
+++ raster_aggregation/exceptions.py
@@ -2,5 +2,5 @@ from rest_framework.exceptions import APIException
 
 
 class MissingQueryParameter(APIException):
-    status_code = 500
+    status_code = 400
     default_detail = 'Missing Query Parameter.'
