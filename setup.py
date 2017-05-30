e356bc79ca6a7574d5fb5f80bfff457119c2305e
Daniel Wiesmann <daniel.wiesmann@gmail.com>
2017-05-30 16:11:34 +0100

    Added django filter to setup requirements.

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
         'djangorestframework>=3.5.4',
         'djangorestframework-gis>=0.11',
         'drf-extensions>=0.3.1',
+        'django-filter==1.0.4',
     ],
     keywords=['django', 'raster', 'gis', 'gdal', 'celery', 'geo', 'spatial'],
     classifiers=[
