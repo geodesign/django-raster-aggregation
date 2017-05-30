b8708c58d6920b829d02e606fd4e14fd210b4015
Daniel Wiesmann <daniel.wiesmann@gmail.com>
2017-05-30 17:44:01 +0100

    Added test project to fix celery configuration.

--- tests/testproj/settings.py
+++ tests/testproj/settings.py
@@ -44,5 +44,6 @@ ROOT_URLCONF = 'raster_aggregation.urls'
 
 CELERY_ALWAYS_EAGER = True
 CELERY_EAGER_PROPAGATES = True
-task_always_eager = True
-task_eager_propagates = True
+
+CELERY_TASK_ALWAYS_EAGER = True
+CELERY_TASK_EAGER_PROPAGATES = True
