dd8423546b701efe265e28428e35547baecbbbf2
Daniel Wiesmann <daniel.wiesmann@gmail.com>
2017-06-01 14:18:29 +0100

    Added postgres and gis contrib apps to installed apps for testing.

--- tests/testproj/settings.py
+++ tests/testproj/settings.py
@@ -12,6 +12,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
+    'django.contrib.gis',
 
     'raster',
     'raster_aggregation',
