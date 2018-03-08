fb86936925c883a1998b6efc5acaf47e6e435465
Daniel Wiesmann <daniel.wiesmann@gmail.com>
2018-03-08 11:44:14 +0000

    Dropped python 2 support.

--- setup.py
+++ setup.py
@@ -35,9 +35,8 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ]
